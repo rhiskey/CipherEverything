430d59ab9bb7a2da4fcdd166df2bffe18e86f0ed
Vladimir Kiselev <first3395@gmail.com>
2022-04-02 17:32:36 +0300

    Initial Commit

---
+++ CipherEverything/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CipherEverything
+//
+//  Created by Владимир Киселев on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
