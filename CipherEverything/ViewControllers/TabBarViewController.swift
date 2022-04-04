f214b9f235a4e3a5b840c28c782235576db372de
Sergey <kopgamerk@gmail.com>
2022-04-04 20:36:48 +0300

    Таббар. Передача в следующие вью

--- CipherEverything/ViewControllers/TabBarViewController.swift
+++ CipherEverything/ViewControllers/TabBarViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
+    var shared = SingleTon.shared
+    var developersList = Developer.getDevelopers()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.hidesBackButton = true
+        
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        for view in self.viewControllers! {
+            if let destination = view as? PasswordsTableViewController {
+                destination.shared = shared
+            } else if let destination = view as? TeamTableViewController {
+                destination.developersList = developersList
+            }
+        }
+    }
+    
 }
