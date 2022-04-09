0e804a73b1d737d4e55682d05b7c29d53908a6dc
PingPongEz <kopgamerk@gmail.com>
2022-04-09 15:35:44 +0300

    основной Синглтон для держания в памяти юзеров.
     Айди увеличивается на 1 каждый раз когда регистрируется новый пользователь
    Получилось вроде как реализовать Список пользователей чтобы получать быстрый доступ к пользователю при авторизации через имеющийся айди. В целом как на любом сайте логин уникален. не реализовал пока что возможность проверки на наличие логина в списке. Но эт не сложно вроде как

--- CipherEverything/Models/UsersData.swift
+++ CipherEverything/Models/UsersData.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class Users {
+    static let usersData = Users()
+    
+    var countOfUsersID = 1
+    
+    var users: [Person] = [Person(name: "Username", password: "Password", accounts: Account.getAccounts(), usersID: 0)]
+    
+    var usersList: [String : Int] = ["Username": 0]
+    
+    
+    private init() {}
+}
