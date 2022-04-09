366732ebe4a64fe20ed8535c6eb2824c70a07ae0
PingPongEz <kopgamerk@gmail.com>
2022-04-09 15:39:30 +0300

    Тупо окно регистрации

--- RegisterViewController.swift
+++ RegisterViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    var sharedUsersData = Users.usersData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerPressed() {
+        sharedUsersData.usersList[userNameTF.text ?? ""] = sharedUsersData.countOfUsersID
+        
+        let newPersonForArray = Person(name: userNameTF.text ?? "", password: passwordTF.text ?? "", accounts: [], usersID: sharedUsersData.countOfUsersID)
+        
+        sharedUsersData.users.append(newPersonForArray)
+        
+        sharedUsersData.countOfUsersID += 1
+        
+        print(sharedUsersData.usersList)
+        
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
