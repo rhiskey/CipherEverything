b8184366baa263c246fcdf9a8a2d634c28030a18
PingPongEz <kopgamerk@gmail.com>
2022-04-09 21:13:49 +0300

    Включил клавишу ретерн на логин вью

--- CipherEverything/ViewControllers/AccountsViewControllers/EditAccountViewController.swift
+++ CipherEverything/ViewControllers/AccountsViewControllers/EditAccountViewController.swift
@@ -16,9 +16,9 @@ class EditAccountViewController: UIViewController, UITextFieldDelegate, Password
     
     // MARK: - Public Properties
     var account: Account!
-    var accountIndex: Int!
     var personsData: Person!
     var usersData = Users.usersData
+    var accountIndex: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -105,7 +105,7 @@ extension EditAccountViewController {
 // MARK: - UITextFieldDelegate
 extension EditAccountViewController: UITextViewDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super .touchesBegan(touches, with: event)
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
     
